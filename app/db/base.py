d7a27ebd854254e8ca861d819deaf74f2516e362
Rahanesh <88672205+Rahanesh@users.noreply.github.com>
2023-08-01 00:38:19 +0330

    Update base.py

--- app/db/base.py
+++ app/db/base.py
@@ -4,7 +4,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args=(
-    {"check_same_thread": False} if SQLALCHEMY_DATABASE_URL.startswith('sqlite') else {}
+    {"check_same_thread": False} if SQLALCHEMY_DATABASE_URL.startswith('mysql') else {}
 ))
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
